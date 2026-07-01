2c8712991d29806d4fe5104538f99c248952dab4
zainab-555 <zainabtanweer08@gmail.com>
2026-07-01 21:34:53 +0530

    git add .  git commit -m"first commit"  git push  git add style.css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-function search(){
-    let city= document.querySelector('.outer input').value
-    const key='d4c2f19e99bb34c2142941b784fd4cfc';
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
-    let res= fetch(url)
-}// api
+// async function search(){
+//     let city= document.querySelector('.box input').value
+//     const key='d4c2f19e99bb34c2142941b784fd4cfc';
+//     const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+//     let res =  await fetch(url)
+//     let data =  await res.json(); // await ka matlan hota hai asynchrounus hota hai 
+
+//     console.log(data);
+// }// api
+async function search() {
+
+    let city = document.querySelector(".box .search-box input").value;
+
+    const key = "d4c2f19e99bb34c2142941b784fd4cfc";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+
+    let res = await fetch(url);
+    let data = await res.json();
+
+    console.log(data);
+
+    document.querySelector(".weather h1").innerHTML = data.main.temp + "°C";
+    document.querySelector(".weather h2").innerHTML = data.name;
+}
+
+document.querySelector(".search-box button").addEventListener("click", search);
